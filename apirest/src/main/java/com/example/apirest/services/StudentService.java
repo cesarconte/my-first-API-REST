29dc95fc3a54e39172387c04022b87d0d0100f7f
cesarconte <yugurta@gmail.com>
2023-08-20 15:54:38 +0200

    StudentService done

---
+++ apirest/src/main/java/com/example/apirest/services/StudentService.java
@@ -0,0 +1,48 @@
+package com.example.apirest.services;
+
+import com.example.apirest.models.Student;
+import com.example.apirest.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student getStudentById(int id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(int id) {
+        studentRepository.deleteById(id);
+    }
+
+    public Student updateStudent(int id, Student updatedStudent) {
+        Student existingStudent = studentRepository.findById(id).orElse(null);
+
+        if (existingStudent != null) {
+            existingStudent.setFirstName(updatedStudent.getFirstName());
+            existingStudent.setLastName(updatedStudent.getLastName());
+            existingStudent.setSubject(updatedStudent.getSubject());
+            existingStudent.setGrade(updatedStudent.getGrade());
+
+            return studentRepository.save(existingStudent);
+        }
+
+        return null;
+    }
+}
+
+
+
